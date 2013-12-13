c645c349187fcea6bd2134314d18c2d83a2555c1
Vojtech Hyza <vhyza@vhyza.eu>
2013-12-13 16:12:40 +0100

    [TESTS] Added test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,21 @@ require 'test/unit'
 require 'shoulda/context'
 require 'turn'
 require 'mocha/setup'
+require 'webmock'
 require 'vcr'
 require 'pry'
 
 require 'pickwick-workers'
+
+VCR.configure do |c|
+  c.cassette_library_dir = File.dirname(__FILE__) + '/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+def fixture_file(name)
+  File.read File.expand_path("../fixtures/#{name}", __FILE__)
+end
+
+def mocked_faraday_response(status, body)
+  stub(status: status, body: body.is_a?(Hash) ? body.to_json : body)
+end
