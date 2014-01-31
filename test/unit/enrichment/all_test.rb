add40c588bc627fcd11a8b8ef74680716a1644d1
Vojtech Hyza <vhyza@vhyza.eu>
2014-01-31 10:58:26 +0100

    [TESTS] Added Enrichment::All test

---
+++ test/unit/enrichment/all_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+module Pickwick
+  module Workers
+    module Enrichment
+      class AllTest < Test::Unit::TestCase
+        context "Enrichment" do
+
+          setup do
+            @job = All.new
+          end
+
+          should "enqueue Geo job" do
+            Geo.expects(:perform_async).with(id: 123)
+            @job.perform(id: 123)
+          end
+
+        end
+      end
+    end
+  end
+end
